c3468bc2ce79bd1aa64bb8fba78ab652cafccc81
jmloa-iscteiul <jmloa@iscte-iul.pt>
2018-10-18 14:24:53 +0100

    Agora dá para procurar Hashtags;

--- ProjectBDA/src/twitter/TwitterApp.java
+++ ProjectBDA/src/twitter/TwitterApp.java
@@ -1,14 +1,15 @@
 package twitter;
 
-import java.util.Iterator;
 import java.util.List;
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterApp {
@@ -18,14 +19,16 @@ public class TwitterApp {
 	
 	public static void main(String[] args) {
 		TwitterApp t = new TwitterApp();
-		t.setCredentials();
-		t.fetchTimeline();
-        
+		t.authenticate();
+		//t.fetchTimeline();
+        //t.searchHashtag("iscte");
+		t.searchPerson("ana");
      }
 	
-	public void setCredentials(){
+	public void authenticate(){
 		
 		try {
+			
         	ConfigurationBuilder cb = new ConfigurationBuilder();
         	cb.setDebugEnabled(true)
         	  .setOAuthConsumerKey("W1f0VvgWPfT8OBqVxvy4Mw")
@@ -34,19 +37,22 @@ public class TwitterApp {
         	  .setOAuthAccessTokenSecret("OahDuXF2Lhl5xlNYALhYZir6xSflAxKP9Zh89T05po");
         	TwitterFactory tf = new TwitterFactory(cb.build());
         	twitter = tf.getInstance();
-		}catch (Exception e) { System.out.println(e.getMessage()); }
+        	
+		}catch (Exception e) { 
+			System.out.println(e.getMessage()); 
+		}
 	}
 	
 	public void fetchTimeline(){
 		        		
 		try{
 			timelineStatuses = twitter.getHomeTimeline();
-            System.out.println("------------------------\n Showing home timeline \n------------------------");
+            System.out.println("|||||||||||||||||||||||| MOSTRAR TIMELINE ||||||||||||||||||||||||");
     		int counter=0;
     		int counterTotal = 0;
             for (Status status : timelineStatuses) {
 				// Filters only tweets from user "catarina"
-				if (status.getUser().getName() != null /* && status.getUser().getName().contains("")*/) {
+				if (status.getUser().getName() != null) {
 					System.out.println(status.getUser().getName() + ":" + status.getText());
 					System.out.println("____________________________________________________________");
 					counter++;
@@ -54,68 +60,49 @@ public class TwitterApp {
 				counterTotal++;
             }
     		System.out.println("-------------\nNº of Results: " + counter+"/"+counterTotal);
-        } catch (Exception e) { System.out.println(e.getMessage()); }
+        } catch (Exception e) { 
+        	System.out.println(e.getMessage()); }
 	}
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*public static void main(String [] args){
-		
-		new TwitterApp().fetchTweets("#Programming");
-		
-	}
-
-	private void fetchTweets(String hashtag) {
-		
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-  	  .setOAuthConsumerKey("W1f0VvgWPfT8OBqVxvy4Mw")
-  	  .setOAuthConsumerSecret("zKH2yAtRyefwsgOO8h8Szc4kru68iEm95QmIG7svw")
-  	  .setOAuthAccessToken("36481851-VhzByC4f9MSsZES1QZQ4e4iBvA9bWGLyv9HKFpy7c")
-  	  .setOAuthAccessTokenSecret("OahDuXF2Lhl5xlNYALhYZir6xSflAxKP9Zh89T05po");
-		
-		TwitterFactory twitterFactory = new TwitterFactory();
-		Twitter twitter = twitterFactory.getInstance();
-		
-		Query query = new Query(hashtag);
-		query.setCount(100);
-		query.setSince("16-10-2018");
-		QueryResult searchResult = null;
+	public void searchHashtag(String hashtag){ // Procura uma hashtag mas so mostra uns poucos resultados
 		
 		try{
-			
-			
-			
-			searchResult = twitter.search(query);
-			
-			List<Status> tweetList = searchResult.getTweets();
-			
-			Iterator<Status> itTweet = tweetList.iterator();
-			
-			while(itTweet.hasNext()){
-				
-				Status tweet = (Status) itTweet.next();
-				
-				System.out.println(tweet.getUser() + " - " + tweet.getText());
-				System.out.println("======================================");
-				
+			Query query = new Query(hashtag);
+			QueryResult result = twitter.search(query);
+			int counter = 0;
+			for(Status status : result.getTweets()){
+				counter++;
+				System.out.println(counter + "-> " + " @" + status.getUser().getScreenName() + ": " + status.getText() + " : " + status.getGeoLocation());
+				System.out.println("-----------------------------------------------------------------------");
 			}
-			
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void searchPerson(String person){
+		
+		try{
+			Query query = new Query(person);
+			ResponseList<User> result = twitter.searchUsers(query.toString(), -1);
+			int counter = 0;
+			System.out.println("estou aqui");
+			for(User user : result){
+				System.out.println("entrei");
+				if(user.getName() != null){
+					System.out.println("aqui depois");
+					counter++;
+					System.out.println(counter + "-> " + user.getName() );
+					System.out.println("----------------------------------");	
+				}
+			}	
 		}catch(TwitterException e){
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 		
 		
-		
 	}
-	*/
 	
 }
