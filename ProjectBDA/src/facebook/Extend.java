780ae1ebff8c1fa424b79b1642bbc5afdaf16f32
rmcmc-iscteiul <rmcmc@iscte-iul.pt>
2018-10-30 15:18:38 +0000

    Criar métodos em vez de ter o código todo em main.

--- ProjectBDA/src/facebook/Extend.java
+++ ProjectBDA/src/facebook/Extend.java
@@ -8,21 +8,42 @@ import com.restfb.FacebookClient;
 import com.restfb.FacebookClient.AccessToken;
 
 public class Extend {
-
-	public static void main(String[] args) {
-		String accessToken = "";
-		FacebookClient fbClient4 = new DefaultFacebookClient(accessToken);
+	
+	private String accessToken = "";
+	FacebookClient fbClient4;
+	
+	public Extend () {
+		fbClient4 = new DefaultFacebookClient(accessToken);
+	}
+	
+	public String getExtension () {
 		AccessToken extendedAccessToken4 = fbClient4.obtainExtendedAccessToken("313621502554589","f2cc137fdfa12d9e959fec3e7be184e9");
-		System.out.println("ExtendedAccessToken: "+extendedAccessToken4.getAccessToken());
-		System.out.println("Expires: " + extendedAccessToken4.getExpires());
 		PrintWriter out;
 		try {
-			out = new PrintWriter("tockens.txt");
-			out.println("Facebook Token " + "\n" + extendedAccessToken4);
+			out = new PrintWriter("tokens.txt");
+			out.println("Facebook Token " + "\n" + extendedAccessToken4.getAccessToken());
+			out.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+		return extendedAccessToken4.getAccessToken();
+	}
+	
+	public static void main(String[] args) {
+//		String accessToken = "EAAEdPLJA8d0BAE7rgGTkhPdfjtEz7kimJHsSJKqj8RR4v7ErVSRft53OyLZBbed8odScy5fZBTZA9e1R4reX4gDa1pA4RtapKGIzUavVZBsFFYQn37ZASV1IKZC27VBfqo9TG9IDk7br9p8We52RnEInHovoCG16gZD";
+//		FacebookClient fbClient4 = new DefaultFacebookClient(accessToken);
+//		AccessToken extendedAccessToken4 = fbClient4.obtainExtendedAccessToken("313621502554589","f2cc137fdfa12d9e959fec3e7be184e9");
+//		System.out.println("ExtendedAccessToken: "+extendedAccessToken4.getAccessToken());
+//		System.out.println("Expires: " + extendedAccessToken4.getExpires());
+//		PrintWriter out;
+//		try {
+//			out = new PrintWriter("tokens.txt");
+//			out.println("Facebook Token " + "\n" + extendedAccessToken4);
+//			out.close();
+//		} catch (FileNotFoundException e) {
+//			e.printStackTrace();
+//		}
+//		
 	}
 	
 }
--- ProjectBDA/src/facebook/postGroups.java
+++ ProjectBDA/src/facebook/postGroups.java
@@ -16,12 +16,18 @@ import com.restfb.types.Group;
 
 public class postGroups {
 
-	public static void main(String[] args) {
 
+	private String accessToken;
+	private FacebookClient fbClient;
+	private Connection<Group> result;
 
-		String accessToken = "";
-		FacebookClient fbClient = new DefaultFacebookClient(accessToken);
-		Connection<Group> result = fbClient.fetchConnection("me/groups",Group.class);
+	public postGroups (String accessToken) {
+		this.accessToken = accessToken;
+		fbClient = new DefaultFacebookClient(accessToken);
+		result = fbClient.fetchConnection("me/groups",Group.class);
+	}
+	
+	public void postar () {
 		Scanner s = new Scanner (System.in);
 		for (List <Group> GroupPage : result) {
 			for (Group aGroup : GroupPage ) {
@@ -61,7 +67,54 @@ public class postGroups {
 				}
 			}
 		}
+	}
 
+	public static void main(String[] args) {
+
+
+//		String accessToken = "";
+//		FacebookClient fbClient = new DefaultFacebookClient(accessToken);
+//		Connection<Group> result = fbClient.fetchConnection("me/groups",Group.class);
+//		Scanner s = new Scanner (System.in);
+//		for (List <Group> GroupPage : result) {
+//			for (Group aGroup : GroupPage ) {
+//				if(aGroup.getName().contains("Iscte") || aGroup.getName().contains("ISCTE") || aGroup.getName().contains("ETI") ) {
+//					System.out.println("Do you want to publish on this group " + aGroup.getName() + "?");
+//					String answer = s.nextLine();
+//					if (answer.equalsIgnoreCase("Yes")) {
+//						System.out.println("Post or upload file?");
+//						String resposta = s.nextLine();
+//						if (resposta.equalsIgnoreCase("post")) {
+//							System.out.println("Write your post");
+//							String post = s.nextLine();
+//							FacebookType response = fbClient.publish(aGroup.getId() + "/feed", FacebookType.class,Parameter.with("message", post));
+//							System.out.println("fb.com"+ response.getId());
+//						}
+//						if (resposta.equalsIgnoreCase("upload file")) {
+//							System.out.println("Write file location");
+//							String attachmentName = s.nextLine();
+//							System.out.println("Write file name");
+//							String name = s.nextLine();
+//							System.out.println("Write description");
+//							String description = s.nextLine();
+//							FileInputStream fis;
+//							try {
+//								fis = new FileInputStream (new File (attachmentName));
+//								FacebookType publish = fbClient.publish(aGroup.getId() + "/photos", FacebookType.class,
+//										BinaryAttachment.with(name, fis), Parameter.with("message", description));
+//							} catch (FileNotFoundException e) {
+//								e.printStackTrace();
+//							}
+//
+//						}
+//
+//					}if (answer.equalsIgnoreCase("fim")) {
+//						System.exit(0);
+//					}
+//				}
+//			}
+//		}
+//
 	}
 
 }
