41146bdb7fcdd46a9aaf53495af5b8270f39e906
jmfs-iscteiul <jmfss1@iscte-iul.pt>
2018-10-30 15:02:08 +0000

    Estrutura base para posts, tweets e mails

---
+++ ProjectBDA/src/commun/standardInfoStruct.java
@@ -0,0 +1,38 @@
+package commun;
+
+public class standardInfoStruct {
+	
+	private String date;
+	private String author;
+	private String title;
+	
+	public standardInfoStruct(String d, String a, String t) {
+		this.date = d;
+		this.author = a;
+		this.title = t;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public void setSender(String author) {
+		this.author = author;
+	}
+
+	public void setContent(String title) {
+		this.title = title;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+}
