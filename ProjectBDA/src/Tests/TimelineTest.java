af591cec4aa5fd1634a985d1285953fb046f3e59
ricardo <ricardo@ricardo-Aspire-VN7-791>
2018-11-21 13:14:48 +0000

    Compatibilizar a classe post com a interface

---
+++ ProjectBDA/src/Tests/TimelineTest.java
@@ -0,0 +1,25 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TimelineTest {
+
+	@Test
+	void testTimeline() {
+		!assertNullc
+	}
+
+	@Test
+	void testSetFilter() {
+		String f = "facebook";
+		setFilter(f);
+	}
+
+	@Test
+	void testGetTimeline() {
+		fail("Not yet implemented");
+	}
+
+}
