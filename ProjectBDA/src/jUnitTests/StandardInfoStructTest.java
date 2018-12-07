1699230492907f5a064a02e22196bd9a169ffa1e
rmcmc-iscteiul <rmcmc@iscte-iul.pt>
2018-12-07 14:42:03 +0000

    JUnit para o package common

---
+++ ProjectBDA/src/jUnitTests/StandardInfoStructTest.java
@@ -0,0 +1,77 @@
+package jUnitTests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import common.standardInfoStruct;
+
+public class StandardInfoStructTest {
+
+	standardInfoStruct s = new standardInfoStruct(new Date (), "Autor", "texto");
+	@Before
+	
+	
+	
+	@Test
+	public void testStandardInfoStruct() {
+		standardInfoStruct s1 = new standardInfoStruct(new Date (), "Autor", "texto");
+		assertNotNull(s1);
+		
+	}
+	
+
+
+	@Test
+	public void testSetSender() {
+		s.setSender("Enviador");
+		assertNotNull(s.getAuthor());
+		assertEquals("Enviador", s.getAuthor());
+		
+	}
+
+	@Test
+	public void testSetContent() {
+		s.setContent("content");
+		assertNotNull(s.getTitle());
+		assertEquals("content", s.getTitle());
+	}
+
+	@Test
+	public void testGetDate() {
+		assertNotNull(s.getDate());
+	}
+
+	@Test
+	public void testGetAuthor() {
+		assertNotNull(s.getAuthor());
+	}
+
+	@Test
+	public void testGetTitle() {
+		assertNotNull(s.getTitle());
+	}
+
+	@Test
+	public void testToString() {
+//		return date + " - " + text;
+		String temp = s.getDate() + " - " + s.getTitle();
+		assertEquals(temp, s.toString());
+	}
+
+	@Test
+	public void testCompareTo() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		standardInfoStruct s2 = new standardInfoStruct(new Date (), "Autor", "texto");
+		assertEquals(-1, s2.compareTo(s));
+	}
+
+}
---
+++ ProjectBDA/src/jUnitTests/XmlTest.java
@@ -0,0 +1,45 @@
+package jUnitTests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.junit.Test;
+
+import common.Xml;
+import common.standardInfoStruct;
+
+public class XmlTest {
+
+	Xml x = new Xml();
+	
+	@Test
+	public void testEscreverXML() {
+		x.escreverXML("Teste", "Junit", "Sucesso");
+	}
+
+	@Test
+	public void testLeituraXML() {
+		assertNull(x.leituraXML("efef", "sdcdscds"));
+		assertNotNull(x.leituraXML("Facebook", "Token"));
+	}
+
+	@Test
+	public void testEscreverDeVarias() {
+		standardInfoStruct s1 = new standardInfoStruct(new Date (), "Autor 1", "texto 1");
+		standardInfoStruct s2 = new standardInfoStruct(new Date (), "Autor 2", "texto 2");
+		ArrayList<standardInfoStruct> lista = new ArrayList<standardInfoStruct>();
+		lista.add(s1);
+		lista.add(s2);
+		x.escreverDeVarias(lista);
+		
+	}
+
+	@Test
+	public void testLerDeVarias() {
+		assertNotNull(x.lerDeVarias());
+	}
+
+}
